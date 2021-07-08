fe9248788df69b4c6b4144a4daae607c1c28bb0b
Jack Zhang <junbojack@gmail.com>
2021-07-08 21:54:00 +0800

    simple tests for pages controller implemented

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
+  describe "GET #autocomplete" do
+    before do
+      get :autocomplete
+    end
+  
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
 
+    it "JSON body response contains expected search qualifiers" do
+      json_response = JSON.parse(response.body)
+      expect(json_response.keys).to match_array(['repos', 'users'])
+    end
+  end
 end
